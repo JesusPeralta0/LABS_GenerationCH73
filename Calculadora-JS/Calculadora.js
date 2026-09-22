fed1eb173b1bd55bbf50eb6a37e330419cae4fb9
Jesus Alvarez <jesusalvarez0637@gmail.com>
2026-09-22 17:13:27 -0600

    Primera calculadora js

---
+++ Calculadora-JS/Calculadora.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Escribe tu nombre:"); 
+let numero_1 = Number(prompt("Escribe el primer número:")); 
+let numero_2 = Number(prompt("Escribe el segundo número:")); 
+
+console.log(`Hola, tu nombre es: ${nombre}
+La suma de ${numero_1} + ${numero_2} es: ${numero_1 + numero_2}
+La multiplicación de ${numero_1} * ${numero_2} es: ${numero_1 * numero_2}
+La resta de ${numero_1} - ${numero_2} es: ${numero_1 - numero_2}
+La división de ${numero_1} / ${numero_2} es: ${numero_1 / numero_2}`);
